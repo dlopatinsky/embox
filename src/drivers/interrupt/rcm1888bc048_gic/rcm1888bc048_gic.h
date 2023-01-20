ec8e5a3364c6d07e45fa7c30a92e58c60eb0f191
Anton Bondarev <anton.bondarev2310@gmail.com>
2023-01-20 17:42:41 +0300

    tmp: drivers: Add rcm1888bc048_gic

---
+++ src/drivers/interrupt/rcm1888bc048_gic/rcm1888bc048_gic.h
@@ -0,0 +1,13 @@
+/**
+ * @file
+ *
+ * @data 26 march 2016
+ * @author: Anton Bondarev
+ */
+
+#ifndef SRC_DRIVERS_INTERRUPT_RCM1888BC048_GIC_H_
+#define SRC_DRIVERS_INTERRUPT_RCM1888BC048_GIC_H_
+
+#define __IRQCTRL_IRQS_TOTAL 128
+
+#endif /* SRC_DRIVERS_INTERRUPT_RCM1888BC048_GIC_H_ */
